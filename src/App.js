5fe2a152fc24a8cee35cfc79bf021224af68a4fa
Bini Babu <binibabu22@gmail.com>
2025-12-11 10:50:43 +0530

    city-country-dropdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+var countries = {
+  India: ["Delhi", "Kerala", "Karnataka"],
+  USA: ["Washington", "Chickago", "NewYork"],
+  Canada: ["Ottawa", "Toronto", "Scarborough"],
+};
 
 function App() {
+  var [country, setCountry] = useState("");
+  var [city, setCity] = useState("");
+
+  function handleCountry(e) {
+    setCountry(e.target.value);
+    setCity("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center" }}>
+      <select value={country} onChange={handleCountry}>
+        {Object.keys(countries).map((c) => {
+          return (
+            <option value={c} key={c}>
+              {c}
+            </option>
+          );
+        })}
+      </select>
+      <select value={city} onChange={(e) => setCity(e.target.value)}>
+        {country &&
+          countries[country].map((c) => {
+            return (
+              <option value={c} key={c}>
+                {c}
+              </option>
+            );
+          })}
+      </select>
+      <h3>Result</h3>
+      {country && city && (
+        <>
+          Selected - {country} -{city}
+        </>
+      )}
     </div>
   );
 }
